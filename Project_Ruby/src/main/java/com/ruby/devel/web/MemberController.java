5796e24d3d96df4cb6742e4c709e7c6fcd2d5490
Joochan_1 <dev.jckim@gmail.com>
2022-06-11 23:57:48 +0900

    update: 회원가입 페이지 추가 - main영역 가변 높이 스크립트를 테스트 하기 위해 layout1_edit.jsp 추가

--- Project_Ruby/src/main/java/com/ruby/devel/web/MemberController.java
+++ Project_Ruby/src/main/java/com/ruby/devel/web/MemberController.java
@@ -1,8 +1,15 @@
 package com.ruby.devel.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
-
+	
+	@GetMapping("/signup")  // 회원가입 페이지
+	public String extend_home()
+	{
+		return "/member/memberaddform";  // m/marketplace/(파일명)
+	}
+		
 }
