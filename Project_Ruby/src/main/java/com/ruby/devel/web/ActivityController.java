10fdb8fdf09d0e555b0b3835ca3f25ca4575b8cd
Wyna <98083311+Wyna2@users.noreply.github.com>
2022-06-13 09:19:01 +0900

    update: activity 페이지/컨트롤러 추가

---
+++ Project_Ruby/src/main/java/com/ruby/devel/web/ActivityController.java
@@ -0,0 +1,15 @@
+package com.ruby.devel.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ActivityController {
+
+	@GetMapping("/activity")  // 메뉴 선택 시 이동하는 기본 페이지
+	public String extend_home()
+	{
+		return "/activity/main";  // activity/(파일명)
+	}
+	
+}
