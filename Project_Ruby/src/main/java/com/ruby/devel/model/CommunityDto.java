4ce2664b6f746550747d439f05a62dc0d8b7d707
Joochan Kim <dev.jckim@gmail.com>
2022-06-29 21:40:09 +0900

    fix: CommunityDto.java 누락된 변경점 반영

--- Project_Ruby/src/main/java/com/ruby/devel/model/CommunityDto.java
+++ Project_Ruby/src/main/java/com/ruby/devel/model/CommunityDto.java
@@ -1,27 +1,31 @@
 package com.ruby.devel.model;
 
 import java.sql.Timestamp;
+
 import org.apache.ibatis.type.Alias;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
 
 @Data
-@Alias("c_dto")
+@Alias("community")
 public class CommunityDto {
-	
 	private String community_idx;
 	private String member_idx;
-	private String tag1;
-	private String tag2;
-	private String tag3;
-	private String subject;
-	private String content;
-	private String photo;
-	private String like_count;
-	private String read_count;
-	private String content_type;
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-	private Timestamp write_day;
-	
+
+	private String tag1; //태그1
+	private String tag2; //태그2
+	private String tag3; //태그3
+	private String subject; //제목
+	private String content; //내용
+	private String photo; //사진
+	private MultipartFile upload;
+	private int like_count; //좋아요 수
+	private int read_count; //조회 수
+	private String content_type; //일반글 or 질문글 분류
+	private Timestamp write_day; //작성날짜
+
+	private int mcount; //댓글 수
+	private String writer; //작성자 받아오기
+
 }
