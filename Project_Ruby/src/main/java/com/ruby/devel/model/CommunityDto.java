281efb977de5750e756632d99d0c8d4df89c3027
jongmin <jm7631@naver.com>
2022-06-29 21:34:01 +0900

    fix: Pull Request 충돌 요소 제거 및 수정

--- Project_Ruby/src/main/java/com/ruby/devel/model/CommunityDto.java
+++ Project_Ruby/src/main/java/com/ruby/devel/model/CommunityDto.java
@@ -1,31 +1,27 @@
 package com.ruby.devel.model;
 
 import java.sql.Timestamp;
-
 import org.apache.ibatis.type.Alias;
-import org.springframework.web.multipart.MultipartFile;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
 @Data
-@Alias("community")
+@Alias("c_dto")
 public class CommunityDto {
+	
 	private String community_idx;
 	private String member_idx;
-	
-	private String tag1; //태그1
-	private String tag2; //태그2
-	private String tag3; //태그3
-	private String subject; //제목
-	private String content; //내용
-	private String photo; //사진
-	private MultipartFile upload;
-	private int like_count; //좋아요 수
-	private int read_count; //조회 수
-	private String content_type; //일반글 or 질문글 분류
-	private Timestamp write_day; //작성날짜
-	
-	private int mcount; //댓글 수
-	private String writer; //작성자 받아오기
+	private String tag1;
+	private String tag2;
+	private String tag3;
+	private String subject;
+	private String content;
+	private String photo;
+	private String like_count;
+	private String read_count;
+	private String content_type;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+	private Timestamp write_day;
 	
 }
--- Project_Ruby/src/main/java/com/ruby/devel/service/impl/MemberMapper.java
+++ Project_Ruby/src/main/java/com/ruby/devel/service/impl/MemberMapper.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.ruby.devel.model.MemberDto;
 
-
 @Mapper
 public interface MemberMapper {
 
@@ -20,19 +19,27 @@ public interface MemberMapper {
 	
 	// 회원가입 추가정보 
 	public int addMemberInfo(MemberDto m_dto);
+	public int signupComplete(String id);
 
 	// 로그인 
 	public int login(HashMap<String, String> user);
+
+	public int checkVaildSignup(HashMap<String, String> user);
 	
-	// 회원 이름 가져오기(by ID)
+	// 회원 Key, 이름, 닉네임 가져오기
 	public String getName(String id);
-	
-	// 회원 이름 가져오기(by ID)
 	public String getNickname(String id);
-	
 	public String getUserKey(String id);
 	
+	// 연락처, 이메일로 아이디 찾기
 	public String findIdByEmail(HashMap<String, String> finder);
-	
 	public String findIdByContactNumber(HashMap<String, String> finder);
+	
+	// 비밀번호 재설정
+	public String searchForResetPw(MemberDto m_dto);
+	public void modMemberPw(MemberDto m_dto);
+	
+	public MemberDto getMemberDatas(String member_idx);
+	public String getMemberAge(String member_idx);
+  
 }
